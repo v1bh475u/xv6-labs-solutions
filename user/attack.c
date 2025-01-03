0ff5606244fe7f12da0890445fccbbb7bbb7a6b3
vibhatsu <maulikbarot2915@gmail.com>
2025-01-03 11:06:26 +0530

    add: attack

--- user/attack.c
+++ user/attack.c
@@ -3,11 +3,12 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
-
+  char *mem = sbrk(PGSIZE * 17);
+  mem = mem + 16 * PGSIZE;
+  write(2, mem + 32, 8);
   exit(1);
 }
